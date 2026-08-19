63b3ce3dfcfcb70566110d66a36c8cd54665d46c
KARAN VERMA <kv4020057@gmail.com>
2026-08-19 15:43:53 +0530

    change api's

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    // origin:'http://localhost:5173',
-    origin:'job-hunter-liard.vercel.app',
+    origin:'http://localhost:5173',
     credentials:true
 }
 
