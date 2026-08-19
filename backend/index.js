acb2eac1d4b78bfd8461566bd95a3269a0b2fe8f
KARAN VERMA <kv4020057@gmail.com>
2026-08-19 13:25:06 +0530

    vercel api applied

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'job-hunter-liard.vercel.app',
     credentials:true
 }
 
