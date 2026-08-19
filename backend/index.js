c562d3336b4e870aa4b5944213e0165296fd66a6
KARAN VERMA <kv4020057@gmail.com>
2026-08-19 12:57:40 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,6 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     origin:'http://localhost:5173',
-    // origin:'https://job-hunter-gauf.onrender.com',
     credentials:true
 }
 
